53e682974908e638095589eb3e6baa33bbaa70f3
Ajay Raj Tiwari <ajprince0607@gmail.com>
2023-05-08 01:21:51 +0530

    fix

--- src/screens/AuthLoading.js
+++ src/screens/AuthLoading.js
@@ -1,35 +1,35 @@
 import React, { useEffect } from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import { View, ActivityIndicator, Text } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginSuccess, loginFailure } from '../store/authSlice';
 import { storeUserToken, retrieveUserToken } from '../store/AsyncStorage';
 
 export default function AuthLoading({ navigation }) {
-  const dispatch = useDispatch();
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+	const dispatch = useDispatch();
+	const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
-  useEffect(() => {
-    const checkToken = async () => {
-      try {
-        const token = await retrieveUserToken();
-        if (token) {
-          dispatch(loginSuccess());
-          navigation.navigate('MyDrawer')
-        } else {
-          dispatch(loginFailure());
-          navigation.navigate('SignIn')
-        }
-      } catch (error) {
-        console.log('Error retrieving user token: ', error);
-      }
-    };
+	useEffect(() => {
+		const checkToken = async () => {
+			try {
+				const token = await retrieveUserToken();
+				if (token) {
+					dispatch(loginSuccess());
+				} else {
+					dispatch(loginFailure());
+					navigation.replace('SignIn');
+				}
+			} catch (error) {
+				console.log('Error retrieving user token: ', error);
+			}
+		};
 
-    checkToken();
-  }, []);
+		checkToken();
+	}, []);
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <ActivityIndicator size="large" color="#0000ff" />
-    </View>
-  );
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text style={{padding:20}}>Fetching please wait</Text>
+			<ActivityIndicator size="large" color="#0000ff" />
+		</View>
+	);
 }
